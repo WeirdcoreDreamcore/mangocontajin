a480e0ca215d770e8f91587dfc8eaf8a77b7303d
WeirdcoreDreamcore <122139149+WeirdcoreDreamcore@users.noreply.github.com>
2023-01-13 22:29:20 -0600

    Update chamoy.lua

--- chamoy.lua
+++ chamoy.lua
@@ -1,9 +1,1 @@
---Aqui hay 3 litros de puro chamoy con mango :)
-local Admin = [1334,23444]
-
-
-
-
-if table.find(Admin, game.Players.LocalPlayer.UserId) then
-   print("Hola")
-  end
+[1334,323079538]
