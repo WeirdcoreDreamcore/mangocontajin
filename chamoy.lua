4f866630ade04084f6a38978b20121fa23b7f230
RoExploits <100005539+RoExploits@users.noreply.github.com>
2023-01-14 11:54:41 -0300

    Update chamoy.lua

--- chamoy.lua
+++ chamoy.lua
@@ -1,1 +1,31 @@
-print("hola")
+local PremUser = {
+12268438,
+  12345,
+123
+
+}
+
+
+if table.find(PremUser, game.Players.LocalPlayer.UserId) then
+print("Whitelisted)
+
+  else
+    
+    
+    game.Players.LocalPlayer.Character.UpperTorso:FindFirstChild('OriginalSize'):Destroy()
+    
+    end
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
